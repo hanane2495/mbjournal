380b8373835d202ad58ea019d10284c4f35ebe89
hanane2495 <48996067+hanane2495@users.noreply.github.com>
2020-04-07 12:49:53 +0100

    Updates

---
+++ precache-manifest.ca095129127c2f30a0f0bf0c4a49a9b5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0021127e7002d6d1ffcf3de5a886c9b4",
+    "url": "/mbjournal/index.html"
+  },
+  {
+    "revision": "dc26b6f1a594fd4b262c",
+    "url": "/mbjournal/static/css/main.3d2492d8.chunk.css"
+  },
+  {
+    "revision": "065a2de1db42ed3dbacb",
+    "url": "/mbjournal/static/js/2.e468baa6.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/mbjournal/static/js/2.e468baa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc26b6f1a594fd4b262c",
+    "url": "/mbjournal/static/js/main.bb84b594.chunk.js"
+  },
+  {
+    "revision": "cff7e709a28fb459ddf1",
+    "url": "/mbjournal/static/js/runtime-main.d90803f0.js"
+  },
+  {
+    "revision": "98aaacb5660d746684fc0145568f0aab",
+    "url": "/mbjournal/static/media/adn.98aaacb5.jpg"
+  },
+  {
+    "revision": "10f3472efccadc0a7fc5e92f6ed1cf20",
+    "url": "/mbjournal/static/media/background5.10f3472e.png"
+  },
+  {
+    "revision": "d11cbca7cde2b3ba61ffa921231d41d6",
+    "url": "/mbjournal/static/media/background6.d11cbca7.png"
+  },
+  {
+    "revision": "3852040ebc1256a9e60e77bcff4525bb",
+    "url": "/mbjournal/static/media/editor.3852040e.jpg"
+  },
+  {
+    "revision": "7815453f43d29e6ad57b909965d3151d",
+    "url": "/mbjournal/static/media/medical bio-08.7815453f.png"
+  },
+  {
+    "revision": "ec189ab04e8ad2f9c553d2a8b2af56c7",
+    "url": "/mbjournal/static/media/medical bio-09.ec189ab0.png"
+  },
+  {
+    "revision": "8df473f86a31f94cdbfc048af061cb1b",
+    "url": "/mbjournal/static/media/virus.8df473f8.jpg"
+  },
+  {
+    "revision": "c54c52f1398494654ed1691b9a690338",
+    "url": "/mbjournal/static/media/virus2.c54c52f1.jpg"
+  },
+  {
+    "revision": "8bcaadbd33f8332ada2ef1547560006c",
+    "url": "/mbjournal/static/media/virus3.8bcaadbd.jpg"
+  }
+]);
